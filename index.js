5499639b21e6ff0bc773b7d40155049034edcd27
mdfaysalbabu <foysalfoysal313@gmail.com>
2023-05-21 23:02:08 +0530

    Update

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    // await client.connect();
+    // await client.connect(); the Code
 
     const carCollection=client.db("carsDB").collection('cars');
     const carsGallery=client.db("carsDB").collection('gallery');
